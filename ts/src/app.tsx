212878e42da033dd24fd706e445423ff5557510c
saejin <saejin@codaru.org>
2026-01-03 17:32:46 -0500

    added minimal routing

--- ts/src/app.tsx
+++ ts/src/app.tsx
@@ -1,27 +1,15 @@
-// css + svgs
+// css
 import "@/assets/css/app.css";
-import Slogan from "@/assets/img/svg/slogan.svg?react";
-// components
-import { Navigation } from "@/components/Navigation";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
+import { LocationProvider, Router } from "preact-iso";
+import Home from "@/pages/Home";
 
 export function App() {
     return (
-        <div class="page">
-            <Header/>
-            
-            <div class="overflow-visible">
-                {/* keep it simple. let each piece speak for itself */}
-                <div class="w-screen h-screen flex flex-col justify-center items-center gap-8">
-                    <Slogan class="fill-brand w-4/5 md:w-[50rem]"/>
-                    <span class="text-center text-xl md:text-4xl z-2 w-3/4"><span class="text-accent">codaru</span> is a non-profit competitive coding league.</span>
-                    <div class="bg-[#d76744] text-brand text-2xl p-2 px-8 cursor-pointer z-2 select-none rounded-sm -translate-x-2 font-black -rotate-2 hover:bg-[#b74724] duration-300 transition-all">join the league</div>
-                </div>
-                <Navigation/>
-            </div>
-            <Footer/>
-        </div>
+        <LocationProvider>
+            <Router>
+                <Home path="/"/>
+            </Router>
+        </LocationProvider>
     );
 }
 
--- ts/vite.config.ts
+++ ts/vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import path from "node:path";
+import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss(), svgr()],
+  plugins: [
+      preact(), 
+      tailwindcss(), 
+      svgr()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "./src")
+      "@": resolve(import.meta.dirname, "./src")
     },
   },
 });
